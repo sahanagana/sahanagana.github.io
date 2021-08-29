435c754ff8a068f746829188a08988fefb05786c
sahanagana <sahaana.ganapathy@gmail.com>
2021-08-29 16:48:30 -0500

    soc button functionality

---
+++ assets/js/main.js
@@ -0,0 +1,18 @@
+// Get the modal
+var modal = document.getElementById("socials");
+
+// Get the button that opens the modal
+var btn = document.getElementById("soc");
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+  document.getElementsByClassName("content").classList.add("in");
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+};
