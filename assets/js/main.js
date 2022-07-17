4b31475c3cbb30e7951718b646fdb48942282b72
sahanagana <70218712+sahanagana@users.noreply.github.com>
2022-07-17 18:04:58 -0500

    button functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,6 +9,7 @@ var ebtn = document.getElementById("ext");
 //Get modal content
 var con = document.getElementsByClassName("content")
 // When the user clicks on the button, open the  associated modal
+//SOCIAL
 sbtn.onclick = function () {
   modal.style.display = "block";
   //change content to social
@@ -20,7 +21,42 @@ sbtn.onclick = function () {
   //insert
   con.classList.add("in");
 };
-
+//RESUME
+rbtn.onclick = function () {
+  modal.style.display = "block";
+  //change content to social
+  con.innerHTML = `
+  <a href = 'https://www.linkedin.com/in/sahana-ganapathy/' class = 'ddlink'><i class='ri-linkedin-line'></i></br>LinkedIn</a> </br></br>
+  <a href = 'https://github.com/sahanagana' class = 'ddlink'><i class='ri-github-line'></i></br>Github</a></br></br>
+  <h4><em>click anywhere else to close</em></h4>
+  `;
+  //insert
+  con.classList.add("in");
+};
+//PROJECTS
+pbtn.onclick = function () {
+  modal.style.display = "block";
+  //change content to social
+  con.innerHTML = `
+  <a href = 'https://www.linkedin.com/in/sahana-ganapathy/' class = 'ddlink'><i class='ri-linkedin-line'></i></br>LinkedIn</a> </br></br>
+  <a href = 'https://github.com/sahanagana' class = 'ddlink'><i class='ri-github-line'></i></br>Github</a></br></br>
+  <h4><em>click anywhere else to close</em></h4>
+  `;
+  //insert
+  con.classList.add("in");
+};
+//EXTRAS
+ebtn.onclick = function () {
+  modal.style.display = "block";
+  //change content to social
+  con.innerHTML = `
+  <a href = 'https://www.linkedin.com/in/sahana-ganapathy/' class = 'ddlink'><i class='ri-linkedin-line'></i></br>LinkedIn</a> </br></br>
+  <a href = 'https://github.com/sahanagana' class = 'ddlink'><i class='ri-github-line'></i></br>Github</a></br></br>
+  <h4><em>click anywhere else to close</em></h4>
+  `;
+  //insert
+  con.classList.add("in");
+};
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function (event) {
   if (event.target === modal) {
